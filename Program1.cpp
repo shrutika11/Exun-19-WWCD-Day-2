4fc363969fc80ea870bf522dfdda949963824489
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 13:15:12 +0530

    Tribo

--- Program1.cpp
+++ Program1.cpp
@@ -1,10 +1,10 @@
-# Tribonacci Series
+# Tribonacci Series - 0, 0, 1, 1, 2, 4...
 
 int tribo(int n){
         if(n == 1)
         return 0;
         if(n == 2)
-        return 1;
+        return 0;
         return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
 }
 
