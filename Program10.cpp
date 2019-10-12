7ca1a4816bc2ccf0e1e252551e0dafea58dae788
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 13:56:35 +0530

    queue

---
+++ Program10.cpp
@@ -0,0 +1,37 @@
+// Delete first element of circular queue
+
+struct node{
+    int data;
+    node* next;
+};
+node* create_circular_queue(void){
+    int n;
+    cout << "Enter number of elements";
+    cin >> n;
+    node* head = NULL, *tail= NULL;
+    while(n--){
+        int a;
+        cout << "Enter element";
+        cin >> a;
+        node* newnode = new node;
+        newnode -> data = a;
+        newnode -> next = NULL;
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            tail -> next = newnode;
+            tail = newnode;
+        }
+    }
+    tail -> next = head;
+    return head;
+}
+node* delete_first_element(node* head){
+    	//implement
+}
+void queue_print(node* head){
+	//implement
+}
+
