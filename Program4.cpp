0e79e3d767696ed404340f53dc7048c8f138372d
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 13:30:30 +0530

    pointer array

---
+++ Program4.cpp
@@ -0,0 +1,17 @@
+// Which line would give an error? Debug the error.
+
+#include<iostream>
+
+using namespace std;
+int arr[10];
+int main(){
+	int arr[5] ={10, 11, 12 , 13, 14};
+	cout << *(arr + 1) << endl;
+	cout << *(arr + 0) << endl;
+	cout << 1[arr] << endl;
+	cout << arr++ << endl; 
+	cout << arr << endl;
+	cout << (arr + 1) << endl;
+	return 0;
+}
+
