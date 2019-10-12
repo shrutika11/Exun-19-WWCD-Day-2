9cc0b3945d0602cf96041a8e23eed7d25bafa835
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 13:18:19 +0530

    Tribo

---
+++ Program1.py
@@ -0,0 +1,10 @@
+# Tribonacci Series - 0, 0, 1, 1, 2, 4...
+
+def tribo(n):
+        if n == 1:
+        return 0;
+        if n == 2:
+        return 0;
+        return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
+
+
