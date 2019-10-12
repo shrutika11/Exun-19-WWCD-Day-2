60164878b10b19125a8882801bc637950542101d
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 14:01:54 +0530

    spiral matrix

---
+++ Program11.cpp
@@ -0,0 +1,27 @@
+// Print matrix in spiral form
+
+void spiralPrint(int m, int n, int a[R][C])
+{
+    int i, k = 0, l = 0;
+    while (k < m & l < n) {
+        for (i = l; i < n; ++i) {
+            cout << a[k][i] << " ";
+        }
+         for (i = k; i < m; ++i) {
+            cout << a[i][n - 1] << " ";
+        }
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << a[m - 1][i] << " ";
+            }
+            m--;
+        }
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
+
