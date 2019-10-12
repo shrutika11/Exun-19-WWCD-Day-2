d932983c9244c076a11f5382fef5cf26d594f120
Jap-Leen <jap.jolly@gmail.com>
2019-10-12 14:10:55 +0530

    binary to decimal

---
+++ Program12.cpp
@@ -0,0 +1,12 @@
+// Binary to Decimal conversion
+
+int function(int n) { 
+    int temp = n,value = 0, c = 1; 
+    while (temp) { 
+        int r = temp % 10;  
+        value += r * c; 
+        c *= 2; 
+    } 
+    return value; 
+}
+
