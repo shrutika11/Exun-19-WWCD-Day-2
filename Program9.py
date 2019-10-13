7454345e131299c5aeccd0406a9c23c7ccca4b21
Jap-Leen <jap.jolly@gmail.com>
2019-10-13 13:01:56 +0530

    quadratic

---
+++ Program9.py
@@ -0,0 +1,10 @@
+# Given quadratic variables a,b and c, find the solution
+
+a = 1
+b = 5
+c = 6
+d = (b*2) - (4*a*c)
+sol1 = (-b-sqrt(d))/(2*a)
+sol2 = (-b+sqrt(d))/(2*a)
+print("""The solution are {} and {}""".format(sol1,sol2))
+
