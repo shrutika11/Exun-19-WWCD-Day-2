33d778628e8cd4e85e7cb05a24daffe60b1f3cbb
Jap-Leen <jap.jolly@gmail.com>
2019-10-13 12:43:10 +0530

    highest freq char

---
+++ Program14.cpp
@@ -0,0 +1,24 @@
+// Highest frequency character in a string
+
+char function(char input[]) {
+    int i,j,m,a[256];
+    char ans;
+    for(i=0;input[i]!='\0';i++){
+        j=input[i];
+        a[j]++;
+    }
+    ans=input[0];
+    j=input[0]; 
+    m=a[j] ;
+
+    for(i=1;input[i]!='\0';i++){
+        j=input[i];
+        if(a[j]>m)
+        { 
+            ans=input[i];
+            m=a[j] ;
+        }
+    }
+    return ans;
+}
+
