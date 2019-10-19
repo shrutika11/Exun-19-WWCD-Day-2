fd4061fd7279fe020ca3e0e6e0b6017b5a447e8d
Natasha <aryanatasha03@gmail.com>
2019-10-19 20:50:35 +0530

    Update Program6.py

--- Program6.py
+++ Program6.py
@@ -1,7 +1,7 @@
 # Print list in reverse order
 # Complexity: E
 
-example = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
+example = [1, 2, 'Exun', 4, 'is', 6, 'Cool'] 
 for i in range(len(example), 0,-1): 
     print(example[i])
 
