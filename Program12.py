e88c5d117bc87dba000c1b9b19e69b81ac85e234
shrutika11 <56750814+shrutika11@users.noreply.github.com>
2019-10-20 09:47:44 +0530

    Update Program12.py

--- Program12.py
+++ Program12.py
@@ -1,8 +1,9 @@
 # Complexity: E
 
+
 def func(x):
    # This function takes a number and prints the factors
-   print("The factors of",x,"are:")
+   print(<colon>The factors of<colon>+str(x)+<colon>are<colon>)
    for i in range(1, x):
        if x % i == 0:
            print(i)
